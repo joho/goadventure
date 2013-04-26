9a16e7782d7c7202796a4411fa6e0679e55984f6
John Barton (joho) <jrbarton@gmail.com>
2013-04-26 14:17:39 +1000

    Extract out tweet generation into a function.

--- fake_twitter_wrapper.go
+++ fake_twitter_wrapper.go
@@ -13,17 +13,23 @@ func (tw *FakeTwitterWrapper) DurationUntilNextRead() time.Duration {
 }
 
 func (tw *FakeTwitterWrapper) GetUserMentionsTimeline() *twittergo.Timeline {
-	user := map[string]interface{}{
-		"screen_name": "johnbarton",
-		"id_str":      "123549854887",
-	}
-	tweet := twittergo.Tweet{
-		"text": "@gotextadventure go north",
-		"user": user,
-	}
+	tweet := createFakeTweetForText("Go North")
+	fmt.Printf("Hypothetically Receive tweet '%v' from '%v'\n", tweet.Text(), tweet.User().ScreenName())
 	return &twittergo.Timeline{tweet}
 }
 
 func (tw *FakeTwitterWrapper) RespondToTweet(tweet *twittergo.Tweet, message string) {
 	fmt.Printf("Hypothetically Send tweet '%v' to '%v'\n", message, tweet.User().ScreenName())
+}
+
+func createFakeTweetForText(tweetText string) twittergo.Tweet {
+	user := map[string]interface{}{
+		"screen_name": "johnbarton",
+		"id_str":      "123549854887",
+	}
+	tweet := twittergo.Tweet{
+		"text": "@gotextadventure " + tweetText,
+		"user": user,
+	}
+	return tweet
 }
